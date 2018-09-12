a3fb95804625cba43156715f15a884054aad4e23
Ezequiel Garcia <ezequiel@collabora.com>
2018-09-12 16:01:45 -0300

    run: Add a --script-dir to run user-defined init-scripts
    This new parameter can be used as:
    
      virtme-run --kdir ~/repos/builds/virtme-x86_64 --script-dir test/
    
    And virtme will run any *.sh script in ./test dir in the guest.
    
    Signed-off-by: Ezequiel Garcia <ezequiel@collabora.com>

--- virtme/commands/run.py
+++ virtme/commands/run.py
@@ -76,6 +76,8 @@ def make_parser():
                    help='Run a one-line shell script in the guest.')
     g.add_argument('--script-exec', action='store', metavar='BINARY',
                    help='Run the specified binary in the guest.')
+    g.add_argument('--script-dir', action='store', metavar='SHELL_COMMAND',
+                   help='Run user init scripts in the guest.')
 
     g = parser.add_argument_group(
         title='Architecture',
@@ -216,8 +218,14 @@ def main():
                   'virtme.guesttools')
 
     initcmds = ['mkdir -p /run/virtme/guesttools',
-                '/bin/mount -n -t 9p -o ro,version=9p2000.L,trans=virtio,access=any virtme.guesttools /run/virtme/guesttools',
-                'exec /run/virtme/guesttools/virtme-init']
+        '/bin/mount -n -t 9p -o ro,version=9p2000.L,trans=virtio,access=any virtme.guesttools /run/virtme/guesttools']
+
+    if args.script_dir is not None:
+        export_virtfs(qemu, arch, qemuargs, os.path.abspath(args.script_dir), 'virtme.userscriptdir')
+        initcmds.append('mkdir -p /run/virtme/user')
+        initcmds.append('/bin/mount -n -t 9p -o ro,version=9p2000.L,trans=virtio,access=any virtme.userscriptdir /run/virtme/user')
+
+    initcmds.append('exec /run/virtme/guesttools/virtme-init')
 
     # Map modules
     if moddir is not None:
